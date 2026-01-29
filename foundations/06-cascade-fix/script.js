ea9c1ea12ece404cd4ed069cab2ae2cdeeb82f77
Trevor Vogel <142553804+twvogel@users.noreply.github.com>
2026-01-29 18:53:46 -0500

    new

---
+++ foundations/06-cascade-fix/script.js
@@ -0,0 +1,56 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+console.log(23 + 97);
+console.log(1 + 2 + 5 + 6 + 7 + 9);
+
+console.log((4 + 6 + 9) / 77);
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+alert ( percentage );
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+
+const badString1 = "This is a test";
+const badString2 = `This is a test, ${string}`;
+const badString3 = 'This is a test';
+console.log(badString1);
+console.log(badString2);
+console.log(badString3);
+
+
+const button = document.querySelector("button");
+
+ function greet() {
+    const name = prompt("What is your name?");
+    const greeting = document.querySelector("#greeting");
+    greeting.textContent = 'Hello${name}, nice to see you!';
+     }
+
+button.addEventListener("click", greet);   
+
+function add7 {x + 7}
+
+
+//Notes
